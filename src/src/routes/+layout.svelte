8c3a9a96e1be9a353191756d8589f67e9ebb7009
TheOnlyWayUp <thedarkdraconian@gmail.com>
2024-04-04 09:40:24 +0000

    fix(): Move meta tags to layout

--- src/src/routes/+layout.svelte
+++ src/src/routes/+layout.svelte
@@ -2,4 +2,17 @@
 	import '../app.pcss';
 </script>
 
+<svelte:head>
+	<meta name="title" content="Panchangam" />
+	<meta
+		name="description"
+		content="View your Tarabalam, Find Auspicious Times, and Avoid Dangerous Times!"
+	/>
+	<meta name="twitter:title" content="Panchangam" />
+	<meta
+		name="twitter:description"
+		content="View your Tarabalam, Find Auspicious Times, and Avoid Dangerous Times!"
+	/>
+</svelte:head>
+
 <slot />
